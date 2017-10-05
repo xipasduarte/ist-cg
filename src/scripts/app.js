428c8c6d30a211899de1415db7ced1e37a30a7bf
Pedro Duarte <xipasduarte@gmail.com>
2017-10-05 22:29:29 +0100

    Remove collisions from the main PR.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,7 +7,6 @@ import onKeyDown from './Events/onKeyDown';
 import onKeyUp from './Events/onKeyUp';
 
 import updateCarPosition from './updateCarPosition';
-import detectCollision from './detectCollision';
 
 const init = () => {
 	// Add state.
@@ -40,7 +39,6 @@ const animate = (step) => {
 	requestAnimationFrame(animate);
 
 	updateCarPosition(step);
-	detectCollision();
 
 	// Update time.
 	gameState.time = step;
