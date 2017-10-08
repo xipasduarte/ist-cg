d7b13016481748eecb0c45a5f4a2bd8c3c1e0de9
Pedro Duarte <xipasduarte@gmail.com>
2017-10-08 17:06:00 +0100

    Correc orthographic camera on resize.

--- src/scripts/Events/onResize.js
+++ src/scripts/Events/onResize.js
@@ -4,7 +4,15 @@ export default (e) => {
   if (window.innerHeight === 0) {
     return;
   }
+
+  if (camera.type === 'OrthographicCamera') {
+    camera.left = window.innerWidth / -10;
+    camera.right = window.innerWidth / 10;
+    camera.top = window.innerHeight / 10;
+    camera.bottom = window.innerHeight / -10;
+  } else {
+    camera.aspect = window.innerWidth / window.innerHeight;
+  }
   
-  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 }
