8ea460f06de25f4a92c554deb943d9016fcf7b67
Pedro Duarte <xipasduarte@gmail.com>
2017-10-21 20:48:30 +0100

    Issue with renaming, case sensitive.

--- src/scripts/updatecontrols.js
+++
@@ -1,6 +0,0 @@
-export default () => {
-    const car = window.scene.getObjectByName('car');
-    const speedControl = document.getElementById('speed');
-
-    speedControl.style.transform = "rotate(" + (Math.abs(Math.floor(car.state.speed)) * 240 / 50 - 210) + "deg)";
-};
