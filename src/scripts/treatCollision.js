3ab23c6c1358592b68de02703b486d4fffc4c80e
Gonçalo <goncalo.a.simoes@tecnico.ulisboa.pt>
2017-10-21 16:53:30 +0100

    cheerio only

--- src/scripts/treatCollision.js
+++ src/scripts/treatCollision.js
@@ -9,40 +9,38 @@ export default () => {
 
 			reference.state.collision.forEach((partnerId) => {
 				const partner = window.scene.getObjectById(partnerId);
-				const partnerOldMovement = new Vector3();
-				partnerOldMovement.copy(partner.state.mov);
+				const referenceOldMovement = new Vector3();
+				referenceOldMovement.copy(reference.state.mov);
 
-				const partnerOldVelocity = Math.abs(partner.state.speed);
+				const referenceOldVelocity = Math.abs(reference.state.speed);
 
 				const collisionVector = new Vector3();
-				collisionVector.copy(partner.position);
 
 				let collisionPartial;
+				//partner to reference velocity exchange
+				collisionVector.copy(reference.position);
+				collisionVector.sub(partner.position);
+				collisionVector.normalize();
+				collisionPartial = Math.abs(collisionVector.dot( partner.state.mov));
 
+				reference.state.mov.add(collisionVector);
+				reference.state.mov.y = 0;
+				reference.state.speed += Math.abs(partner.state.speed * collisionPartial);
+				reference.state.mov.normalize();
+
+				if (partner.name === 'car') {
+					return;
+				}
 				// Reference to partner velocity exchange.
+				collisionVector.copy(partner.position);
 				collisionVector.sub(reference.position);
 				collisionVector.normalize();
-				collisionPartial = Math.abs(collisionVector.dot(reference.state.mov));
+				collisionPartial = Math.abs(collisionVector.dot(referenceOldMovement));
 
 				partner.state.mov.add(collisionVector);
 				partner.state.mov.y = 0;
 				partner.state.mov.normalize();
-				partner.state.speed += Math.abs(reference.state.speed * collisionPartial);
-
-				//partner to reference velocity exchange
-				if (partner.name === 'car') {
-					return;
-				}
-				
-				collisionVector.copy(reference.position);
-				collisionVector.sub(partner.position);
-				collisionVector.normalize();
-				collisionPartial = Math.abs(collisionVector.dot( partnerOldMovement));
-
-				reference.state.mov.add(collisionVector);
-				reference.state.mov.y = 0;
-				reference.state.speed += Math.abs(partnerOldVelocity * collisionPartial);
-				reference.state.mov.normalize();
+				partner.state.speed += Math.abs(referenceOldVelocity * collisionPartial);
 
 				//cheerio on cheerio, reduce speed
 				reference.state.speed = reference.state.speed *(1-0.3);
