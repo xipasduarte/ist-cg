7d6732d953c9bb023a54909d9b11030f259ae375
Gonçalo Simões <goncalo.a.simoes@tecnico.ulisboa.pt>
2017-11-01 14:15:19 +0000

    velas added, need key

--- src/scripts/Renderer.js
+++ src/scripts/Renderer.js
@@ -1,9 +1,10 @@
-import { WebGLRenderer } from 'three';
+import { WebGLRenderer, PCFSoftShadowMap} from 'three';
 
 export default () => {
 	const renderer = new WebGLRenderer();
 
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.shadowMap.enabled = true;
 	
   window.renderer = renderer;
 }
